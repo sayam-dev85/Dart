4df3b318632c79b81b4dc3cdf686fefe2772df3f
Md. Abu Sayam <abusayam.dev@gmail.com>
2025-08-23 23:04:30 +0600

    Age Verification

---
+++ bin/02 String & Boolean/age_verification.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name = "Abu Sayam";
+  int age = 24;
+
+  bool isAdult = age >= 18;
+  if(isAdult){
+    print("$name is an adult.");
+  }
+  else{
+    print("$name is not an adult.");
+  }
+}
