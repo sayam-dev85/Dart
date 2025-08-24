0fbe120ab5f28f83fa52423109c46c197a54289c
Md. Abu Sayam <abusayam.dev@gmail.com>
2025-08-24 20:35:48 +0600

    Sum of all Elements

--- bin/Lists/sum_of_all_elements.dart
+++ bin/Lists/sum_of_all_elements.dart
@@ -1,6 +1,11 @@
 void main (){
   var numbers = [1, 2, 3, 4, 5,];
-  var sum = numbers.reduce((a, b) => a + b);
-  print(sum);
+  // var sum = numbers.reduce((a, b) => a + b);
+  // print(sum);
 
+  int  sum = 0;
+  for(var num in numbers){
+    sum  = sum + num;
+  }
+  print(sum);
 }
