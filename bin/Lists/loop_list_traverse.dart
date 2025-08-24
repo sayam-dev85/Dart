371f873b872254550b91b73f3cfccba937c02203
Md. Abu Sayam <abusayam.dev@gmail.com>
2025-08-24 10:28:49 +0600

    List - Loop

---
+++ bin/Lists/loop_list_traverse.dart
@@ -0,0 +1,24 @@
+//for loop
+/*
+void main (){
+  var fruits = ["Apple", "Mango", "Banana"];
+  for(int i = 0; i < fruits.length; i++){
+    print(fruits[i]);
+  }
+}*/
+
+
+// for in Loop
+/*void main (){
+  var fruits = ["Apple", "Mango", "Banana"];
+  for(var fruit in fruits){
+    print(fruit);
+  }
+
+}*/
+
+// forEach
+void main (){
+  var fruits = ["Apple", "Mango", "Banana"];
+  fruits.forEach((fruit) => print(fruit));
+}
